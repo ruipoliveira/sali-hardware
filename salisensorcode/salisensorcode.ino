20eacd3dac2b9686c99be0e95e272d9e4e847027
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-03-29 17:12:31 +0100

    atualizado script par acorrer no rasp... ou outros.... mas em python

--- salisensorcode/salisensorcode.ino
+++ salisensorcode/salisensorcode.ino
@@ -1,26 +1,33 @@
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
 }
 
+int port_luminosity = 5;
+int port_temperature = 2; 
+
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("das");
-  delay(1);        // delay in between reads for stability
+  //Serial.println(readTemperature(port_temperature));
+  Serial.println("fsdfds"); 
+  
+  delay(60);        // delay in between reads for stability
   
 }
 
-int readTemperature(int x, int y){
-  int result;
-  result = x * y;
-  return result;
+int readTemperature(int port){
+  int value = analogRead(port);
+
+  return value;
 }
 
-int readLuminosity(int x, int y){
-  int result;
-  result = x * y;
-  return result;
+int readLuminosity(int port){
+  int value = analogRead(port);
+
+  return value;
 }
 
 int readLiquidLevel(int x, int y){
---
+++ script.py
@@ -0,0 +1,31 @@
+####################################
+# File name: script.py             #
+# Version: 1                       #
+# Author: Rui Oliveira             #
+# Email: ruipedrooliveira@ua.pt    #
+# Date create: 29-03-2017          #
+# Date last modified: 29-03-2017   #
+# Python Version: 2.7              #
+####################################
+
+import requests
+import json
+
+def post_value(value, id_sensor):
+	authentication = ('roliveira', 'roliveira1993194')
+	login_payload = {'value': value}
+	url = 'http://127.0.0.1:8000/api/reading/'+str(id_sensor)
+	response = requests.post(url, data=login_payload,auth=authentication)
+
+
+# type_info: seding_time status_sm baterry_sm
+def get_info_sm(id_sm_or_name, type_info): # in minutos
+	url = 'http://127.0.0.1:8000/api/sm/'+str(id_sm_or_name)
+	response = requests.get(url)
+	data = response.json()
+	print data[str(type_info)]
+
+
+post_value(1213,47)
+#
+#get_info_sm('arduino_nano','seding_time')

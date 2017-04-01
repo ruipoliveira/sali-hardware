8ab0199df452e321e187e4dd192c8413a50c532b
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-01 18:22:12 +0100

    ..

--- salisensorcode/salisensorcode.ino
+++ salisensorcode/salisensorcode.ino
@@ -6,38 +6,55 @@ void setup() {
 
 }
 
-int port_luminosity = 5;
-int port_temperature = 2; 
+int port_luminosity = 4;
+int port_temperature = 7;
+int port_level = 1; 
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println(readTemperature(port_temperature));
-  Serial.println("fsdfds"); 
+  Serial.println(readLuminosity(port_luminosity));
+  
+  //Serial.println(readLiquidLevel(port_level));
+  //Serial.println(readSoilMoisture(5)); 
   
   delay(60);        // delay in between reads for stability
   
 }
 
 int readTemperature(int port){
+
+  int Vo;
+  float R1 = 10000;
+  float logR2, R2, T, Tc, Tf;
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+  Vo = analogRead(port);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  Tc = T - 273.15;
+  Tf = (Tc * 9.0)/ 5.0 + 32.0; 
+ 
+  return Tc;
+}
+
+long readLuminosity(int port){
   int value = analogRead(port);
-
-  return value;
+  long perc = (value*100L)/1023; 
+  return perc;
 }
 
-int readLuminosity(int port){
+int readLiquidLevel(int port){
   int value = analogRead(port);
-
-  return value;
+  
+  if (value == 0)
+    return 0;
+  else
+    return 1;
 }
 
-int readLiquidLevel(int x, int y){
-  int result;
-  result = x * y;
-  return result;
-}
-
-int readSoilMoisture(int x, int y){
-  int result;
-  result = x * y;
-  return result;
+int readSoilMoisture(int port){
+  return digitalRead(3);
 }

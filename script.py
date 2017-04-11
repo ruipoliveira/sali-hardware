bbe7cae070b4a4e50b4d8169edf85ee8f273b373
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-11 17:53:10 +0100

    update script. acess api with autent check!

--- script.py
+++ script.py
@@ -15,9 +15,8 @@ import json
 from time import sleep
 import serial
 
-
 def post_value(value, id_sensor):
-	authentication = ('roliveira', 'roliveira1993194')
+	authentication = ('roliveira', 'ro1993194')
 	login_payload = {'value': value}
 	url = 'http://127.0.0.1:8000/api/reading/'+str(id_sensor)
 	response = requests.post(url, data=login_payload,auth=authentication)
@@ -25,10 +24,10 @@ def post_value(value, id_sensor):
 
 # type_info: seding_time status_sm baterry_sm
 def get_info_sm(id_sm_or_name, type_info): # in minutos
-	url = 'http://127.0.0.1:8000/api/sm/'+str(id_sm_or_name)
-	response = requests.get(url)
+	url = 'http://127.0.0.1:8000/api/cm/'	
+	response = requests.get(url, headers={'Authorization': 'Token 7467cb944f696b9d848a78c68025468cb962f68a'} )
 	data = response.json()
-	print data[str(type_info)]
+	print data#[str(type_info)]
 
 
 #post_value(1213,47)
@@ -38,8 +37,13 @@ def get_info_sm(id_sm_or_name, type_info): # in minutos
 
 #read usb port 
 
-# Establish the connection on a specific port
-ser = serial.Serial('/dev/ttyUSB0', 9600) 
 
-while True:
+def read_arduino(pathUSB)
+	# Establish the connection on a specific port
+	ser = serial.Serial(pathUSB, 9600) 
+
 	print ser.readline() # Read the newest output 
+
+
+read_arduino('/dev/ttyUSB0')
+get_info_sm('dsa','name')

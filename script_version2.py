8bfb6d2430b6058ddf68397871c1fdfa498d5c5d
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-05-22 18:42:00 +0100

    send info

--- script_version2.py
+++ script_version2.py
@@ -1,5 +1,6 @@
 
 import bluetooth
+import socket
 
 target_name = "HC-06"
 target_address = None
@@ -15,3 +16,16 @@ if target_address is not None:
     print ("found target bluetooth device with address "+ target_address)
 else:
     print ("could not find target bluetooth device nearby")
+
+
+port = 3
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((target_address,port))
+print ("ligado...")
+#while 1:
+#    text = input()
+#    if text == "quit":
+#        break
+#    s.send(bytes(text, 'UTF-8'))
+s.close()
+

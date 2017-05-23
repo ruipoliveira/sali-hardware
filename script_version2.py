2753895e3225bd8c2c591bf2469ab693bda08f8f
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-05-23 10:02:46 +0100

    test receive data

--- script_version2.py
+++ script_version2.py
@@ -25,10 +25,19 @@ for services in bluetooth.find_service(address = target_address):
 sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
 sock.connect((target_address,port))
 
+sock.bind(("",port))
+sock.listen(1)
+client_sock,address = sock.accept()
+
 print ("connection established...")
 
 while 1:
     text = input()
+
+    if text == "2": # receive data
+    	data = client_sock.recv(1024)
+		print ("received [%s]" % data)
+
     if text == "quit":
         break
     sock.send(bytes(text, 'UTF-8'))

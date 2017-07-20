5b3ddd757021ea260f5acefdd205d7b13325a986
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-07-20 22:05:59 +0100

    up

--- script.py
+++ script.py
@@ -1,24 +1,44 @@
+####################################
+# File name: script.py             #
+# Version: 1                       #
+# Author: Rui Oliveira             #
+# Email: ruipedrooliveira@ua.pt    #
+# Date create: 29-03-2017          #
+# Date last modified: 20-07-2017   #
+# Python Version: 2.7              #
+####################################
 
+#!/usr/bin/python
 
+import time, sys
+from threading import Thread
+import bluetooth
+import socket
 import requests
 import json
 from time import sleep
-import time 
-import serial
-from serial import Serial, SEVENBITS, STOPBITS_ONE, PARITY_EVEN
-from unidecode import unidecode
 import string
 import random
-import commands
 
 
 auth={'Authorization': 'Token a9f6b9abaa2519650a7625d78c3f52eb1c629f08'}
 
-id_sensor_temperature = 8
-id_sensor_level = 10
-id_sensor_luminosity = 9
-id_sensor_valve = 11
-id_sm = 5 
+id_sensor_temperature = 15
+id_sensor_level = 17
+id_sensor_luminosity = 16
+id_sensor_valve = 18
+id_sm = 8
+id_sensor_valve = 18
+
+target_name = "HC-06"
+target_address = None 
+port = 1
+
+def get_seding_time(id_sm): # in minutos
+	url = 'http://192.168.160.20/api/sm/'+str(id_sm)
+	response = requests.get(url, headers=auth )
+	data = response.json()
+	return data['seding_time']
 
 
 def read_value_in_sensor(value, id_sensor):
@@ -27,104 +47,95 @@ def read_value_in_sensor(value, id_sensor):
 	response = requests.post(url, 
 		data=login_payload,
 		headers=auth)
-
-
-def get_baterry_percentage(): 
-    
-    c = 'upower -i /org/freedesktop/UPower/devices/battery_BAT0'
-    output = commands.getstatusoutput(c)
-    
-    after_perc = output[1].split('percentage: ')[1].strip()
-
-    index = after_perc.find('%')
-
-    return after_perc[:index]
-
-
-def get_seding_time(id_sm): # in minutos
-	url = 'http://192.168.160.20/api/sm/'+str(id_sm)
+	
+def get_status_valve(id_sensor_valve): # in minutos
+	url = 'http://192.168.160.20/api/sensor/'+str(id_sensor_valve)
 	response = requests.get(url, headers=auth )
 	data = response.json()
-	return data['seding_time']
 	
+	state = 0 
+	if str(data[0]['status_actuator']) == "True": 
+		state = 1
+
+	return state
 
 
-def read_data_arduino(pathUSB):
-	ser = serial.Serial(
-	    port=pathUSB,
-	    baudrate=9600,
-	    parity=serial.PARITY_ODD,
-	    stopbits=serial.STOPBITS_TWO,
-	    bytesize=serial.SEVENBITS
-	)
-
-
-	ser.readline() 
-	while True:
-		#ser.write(str(random.randint(0, 1)))
-		print ser.readline() 
-		#print ser.readline()
-
+def controllerValve(sock):
+	last_status =0
+	while 1 :
+		status = get_status_valve(id_sensor_valve)
+		#status = random.getrandbits(1) # with debug
 		
-		# <TEMPERATURE>;<LEVEL WATER>;<LUMINOSITY>;<WATER VALVE> 
+		#send status if status is not equal last status 		
+		if status != last_status: 
+			sock.send(bytes(str(status), 'UTF-8'))
+			last_status=status
+			print ("Send.. "+str(status)) 
+		
+		#time.sleep(1) #debug
 
-		t = time.strftime("%Y-%m-%d %H:%M")
+def receiveData(sock,request_data_op):
+	while 1 :
+		sock.send(bytes(request_data_op, 'UTF-8'))
+		print ("request data")
 
-
-		data_split = ser.readline().split(';')
-
-
-		print time.strftime("%Y-%m-%d %H:%M")
-		print 'TEMPERATURE = '+str(data_split[0])
-		read_value_in_sensor(data_split[0], id_sensor_temperature)
-
-		print 'LEVEL WATER = '+str(data_split[1])
-		read_value_in_sensor(data_split[1], id_sensor_level)
-
-		print 'LUMINOSITY = '+str(data_split[2])
-		read_value_in_sensor(data_split[2], id_sensor_luminosity)
-
-		print 'WATER VALVE = '+str(data_split[3])
-		read_value_in_sensor(data_split[3], id_sensor_valve)
-
-		print 'BATERIA: '+str(get_baterry_percentage()) +'%'
-
-		print '============================='
+		data = ""	
+		while 1:
+			try:
+				data += sock.recv(1024).decode('utf-8')
+				data_end = data.find('\n')
+				if data_end != -1:
+					rec = data[:data_end]
+					data_split = data.rstrip().split(';')
+					print (time.strftime("%Y-%m-%d %H:%M"))
+					print ("TEMPERATURE = "+str(data_split[0]))
+					read_value_in_sensor(data_split[0], id_sensor_temperature)
+					print ('LEVEL WATER = '+str(data_split[1]))
+					read_value_in_sensor(data_split[1], id_sensor_level)
+					print ('LUMINOSITY = '+str(data_split[2]))
+					read_value_in_sensor(data_split[2], id_sensor_luminosity)
+					print ('WATER VALVE = '+str(data_split[3]))
+					read_value_in_sensor(data_split[3], id_sensor_valve)
+					print ('=============================')
+					data = data[data_end+1:]
+					break 
+			except KeyboardInterrupt:
+				break
 
 		time.sleep(60*get_seding_time(id_sm))
 
 
+if __name__ == "__main__":
 
+	nearby_devices = bluetooth.discover_devices()
 
+	for bdaddr in nearby_devices:
+	    if target_name == bluetooth.lookup_name( bdaddr ):
+	        target_address = bdaddr
+	        break
 
+	if target_address is not None:
+	    print ("found target bluetooth device with address "+ target_address)
+	else:
+	    print ("could not find target bluetooth device nearby")
+	    sys.exit(0)
 
-#post_value(1213,1)
-#
-#get_info_sm('arduino_nano','seding_time')
+	for services in bluetooth.find_service(address = target_address):
+		print (" Port: %s" % (services["port"]))
 
-
-#read usb port 
-
-
+	sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
+	sock.connect((target_address,port))
 	
 
-######## to run... ########
-read_data_arduino('/dev/ttyUSB0')
-#print get_baterry_percentage()
+	print ("connection established...")
+	request_data_op = "2"
 
+	thread_controller = Thread(target=controllerValve, args=[sock])
 
+	thread_receiveData = Thread(target=receiveData, args=[sock,request_data_op])
 
-######## testing... ########
+	print ("Start thread controller")
+	thread_controller.start()
 
-
-#get_info_sm(); 
-
-#get_seding_time(2)
-
-
-#while True:
-#	print time.strftime("%Y-%m-%d %H:%M")
-#	read_value_in_sensor(random.randint(0, 100), id_sensor_temperature)
-#	read_value_in_sensor(random.randint(0, 100), id_sensor_luminosity)
-#	print "outro...."
-#	time.sleep(60*get_seding_time(2))
+	print ("Start thread receive data")
+	thread_receiveData.start()

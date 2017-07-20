f0eab54cae7fa134fb6d2cfa614c273a885597d6
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-07-20 11:49:26 +0100

    upp

--- upvalve.py
+++ upvalve.py
@@ -1,7 +1,16 @@
+import requests
+import json
+from time import sleep
+import time 
 import serial
+from serial import Serial, SEVENBITS, STOPBITS_ONE, PARITY_EVEN
+from unidecode import unidecode
+import string
 import random
-import sys
+import commands
 
+
+"""
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=9600,
@@ -11,3 +20,29 @@ ser = serial.Serial(
 )
 
 ser.write(str(sys.argv[1]))
+
+
+
+"""
+auth={'Authorization': 'Token a9f6b9abaa2519650a7625d78c3f52eb1c629f08'}
+
+id_sensor_valve = 18
+
+id_sm = 5 
+
+
+
+def get_status_valve(id_sensor_valve): # in minutos
+	url = 'http://192.168.160.20/api/sensor/'+str(id_sensor_valve)
+	response = requests.get(url, headers=auth )
+	data = response.json()
+	
+	state = 0 
+	if str(data[0]['status_actuator']) == "True": 
+		state = 1
+
+	return state
+
+
+
+print get_status_valve(id_sensor_valve)
--- withthreads.py
+++ withthreads.py
@@ -1,5 +1,17 @@
+####################################
+# File name: script.py             #
+# Version: 1                       #
+# Author: Rui Oliveira             #
+# Email: ruipedrooliveira@ua.pt    #
+# Date create: 29-03-2017          #
+# Date last modified: 20-07-2017   #
+# Python Version: 2.7              #
+####################################
 
-import time
+#!/usr/bin/python
+
+
+import time, sys
 from threading import Thread
 import bluetooth
 import socket
@@ -9,6 +21,7 @@ from time import sleep
 import string
 import random
 
+
 auth={'Authorization': 'Token a9f6b9abaa2519650a7625d78c3f52eb1c629f08'}
 
 id_sensor_temperature = 15
@@ -16,6 +29,7 @@ id_sensor_level = 17
 id_sensor_luminosity = 16
 id_sensor_valve = 18
 id_sm = 8
+id_sensor_valve = 18
 
 target_name = "HC-06"
 target_address = None 
@@ -35,18 +49,33 @@ def read_value_in_sensor(value, id_sensor):
 		data=login_payload,
 		headers=auth)
 	
+def get_status_valve(id_sensor_valve): # in minutos
+	url = 'http://192.168.160.20/api/sensor/'+str(id_sensor_valve)
+	response = requests.get(url, headers=auth )
+	data = response.json()
+	
+	state = 0 
+	if str(data[0]['status_actuator']) == "True": 
+		state = 1
+
+	return state
+
 
 def controllerValve(sock):
     while 1 :
-	    print ("eiii controle ")
-	    time.sleep(20)
+
+	    state = get_status_valve(id_sensor_valve)
+
+		sock.send(bytes(status, 'UTF-8'))
+		print  ("send status")
+	    #time.sleep(20)
 
 def receiveData(sock):
 	while 1 :
 		sock.send(bytes(request_data, 'UTF-8'))
 		print ("request data")
 
-		data = ""
+		data = ""	
 		while 1:
 			try:
 				data += sock.recv(1024).decode('utf-8')
@@ -85,6 +114,7 @@ if __name__ == "__main__":
 	    print ("found target bluetooth device with address "+ target_address)
 	else:
 	    print ("could not find target bluetooth device nearby")
+	    sys.exit(0)
 
 	for services in bluetooth.find_service(address = target_address):
 		print (" Port: %s" % (services["port"]))
